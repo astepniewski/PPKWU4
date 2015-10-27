89c870645d8d4fd3c5297644336c027a2c127a11
astepniewski <a.stepniewski@hotmail.com>
2015-10-27 19:16:18 +0100

    ZipManagerCompressedFileContent red

--- Biblioteka1Tests/ZipManagerTest.cs
+++ Biblioteka1Tests/ZipManagerTest.cs
@@ -25,5 +25,31 @@ namespace Biblioteka1Tests
             Directory.Delete(dirName);
             File.Delete(zipPath);
         }
+
+        [TestMethod]
+        public void CompressFileToZipAndCheckContentOfThisArchive()
+        {
+            string dirName = "dir";
+            string filePath = dirName + @"\testfile.txt";
+            string zipPath = "archive.zip";
+
+            Directory.CreateDirectory(dirName);
+
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                sw.WriteLine("This");
+                sw.WriteLine("is some text");
+                sw.WriteLine("to test");
+                sw.WriteLine("Reading");
+            }
+            ZipManager zipManager = new ZipManager();
+
+            zipManager.CompressToZip(dirName, zipPath);
+
+            Assert.IsTrue(File.Exists(zipPath));
+
+            Directory.Delete(dirName);
+            File.Delete(zipPath);
+        }
     }
 }
