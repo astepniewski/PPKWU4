2fe8db1fd041046584bc31cd5b0f197180b4caf5
astepniewski <a.stepniewski@hotmail.com>
2015-10-27 19:02:39 +0100

    Test case CompressFileToZipAndCheckZipFileExists for ZipManager from Biblioteka1

---
+++ Biblioteka1Tests/ZipManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Biblioteka1;
+
+namespace Biblioteka1Tests
+{
+    [TestClass]
+    public class ZipManagerTest
+    {
+        [TestMethod]
+        public void CompressFileToZipAndCheckZipFileExists()
+        {
+            string dirName = "dir";
+            string zipPath = "archive.zip";
+
+            Directory.CreateDirectory(dirName);
+
+            ZipManager zipManager = new ZipManager();
+
+            zipManager.CompressToZip(dirName, zipPath);
+
+            Assert.IsTrue(File.Exists(zipPath));
+
+            Directory.Delete(dirName);
+            File.Delete(zipPath);
+        }
+    }
+}
