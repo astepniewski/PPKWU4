1a63a892e036a9ab8fd328c1dbc3611dc253d0c4
matihuf <matihuf@hotmail.com>
2015-10-27 18:58:04 +0100

    add ControlSumTestClass

---
+++ Biblioteka2Tests/ControlSumTestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Biblioteka2Tests
+{
+    [TestClass]
+    public class ControlSumTestClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
