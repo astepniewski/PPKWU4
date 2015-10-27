23e57fe9a8b8f1ad9fdfe6ffdafd1009dfc1d4c5
matihuf <matihuf@hotmail.com>
2015-10-27 18:33:49 +0100

    add EncryptedTestClass, add test FilePathIsNull

---
+++ Biblioteka2Tests/EncryptedTestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Biblioteka2;
+
+namespace Biblioteka2Tests
+{
+    [TestClass]
+    public class EncryptedTestClass
+    {   
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FilePathIsNull()
+        {
+            Encryption encryption = new Encryption();
+            encryption.EncryptFile(null, "encrypted.txt", "password");
+        }
+    }
+}
