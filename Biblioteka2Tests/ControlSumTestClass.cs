70d1442a3f5f59d7aba403a96c3bbdef10645b3f
matihuf <matihuf@hotmail.com>
2015-10-27 19:01:27 +0100

    add test FilePathIsNull_MD5sum

--- Biblioteka2Tests/ControlSumTestClass.cs
+++ Biblioteka2Tests/ControlSumTestClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Biblioteka3;
 
 namespace Biblioteka2Tests
 {
@@ -7,8 +8,11 @@ namespace Biblioteka2Tests
     public class ControlSumTestClass
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FilePathIsNull_MD5sum()
         {
+            ControlSum controlSum = new ControlSum();
+            controlSum.MD5SUm(null);
         }
     }
 }
